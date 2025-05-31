e7eae202ac6e970b5a053fd84524dc5a0d45f90a
birajchhetri30 <birajchhetri3824@gmail.com>
2025-05-31 23:04:05 +0545

    Categories displayed on notes

--- Frontend/src/components/Note.jsx
+++ Frontend/src/components/Note.jsx
@@ -1,7 +1,24 @@
 import { MdEdit } from "react-icons/md";
 import { MdDelete } from "react-icons/md";
+import { useEffect, useState } from "react";
+import api from "../services/api";
 
 export default function Note({ note, onEdit, onDelete }) {
+    const [categories, setCategories] = useState([]);
+    const [error, setError] = useState('');
+
+    useEffect(() => {
+        const fetchNoteCategories = async () => {
+            try {
+                const res = await api.get(`/categories/notes/${note.id}/categories`);
+                setCategories(res.data);
+            } catch (err) {
+                setError('Failed to load categories');
+            }
+        };
+        fetchNoteCategories();
+    }, [note.id]);
+
     return (
         <div
             className="relative flex flex-col justify-between h-[300px] p-4 bg-primary-300 border-3 border-secondary-300 rounded-[8px] shadow-lg
@@ -11,7 +28,7 @@ export default function Note({ note, onEdit, onDelete }) {
         >
             <div>
                 <h3
-                    className="text-2xl text-secondary-400 overflow-hidden"
+                    className="text-2xl text-secondary-400 line-clamp-2"
                 >
                     {note.title}
                 </h3>
@@ -19,30 +36,45 @@ export default function Note({ note, onEdit, onDelete }) {
                 <hr className="border border-secondary-100 my-1" />
 
                 <p
-                    className="text-md text-secondary-400 mt-2 overflow-hidden"
+                    className="text-md text-secondary-400 mt-2 line-clamp-4"
                 >
                     {note.content}
                 </p>
             </div>
 
-            <div className="flex items-center justify-between">
-                <small
-                    className="text-secondary-300"
-                >
-                    {new Date(note.updated_at).toLocaleString()}
-                </small>
+            <div className="flex flex-col">
 
-                <div className="flex gap-2">
-                    <MdEdit
-                        className="text-secondary-400 cursor-pointer text-xl hover:brightness-170"
-                        onClick={() => onEdit(note)}
+                <div className="mb-1 w-4/5 overflow-hidden whitespace-nowrap text-ellipsis">
+                    {categories.map((cat) => (
+                        <span
+                            key={cat.id}
+                            className="text-sm px-1 py-1 mr-2 bg-secondary-100 text-secondary-400 rounded-md"
+                        >
+                            {cat.name}
+                        </span>
+                    ))}
 
-                    />
+                </div>
 
-                    <MdDelete
-                        className="text-secondary-400 cursor-pointer text-xl hover:brightness-170"
-                        onClick={() => onDelete(note.id)}
-                    />
+                <div className="flex items-center justify-between">
+                    <small
+                        className="text-secondary-300"
+                    >
+                        {new Date(note.updated_at).toLocaleString()}
+                    </small>
+
+                    <div className="flex gap-2">
+                        <MdEdit
+                            className="text-secondary-400 cursor-pointer text-xl hover:brightness-170"
+                            onClick={() => onEdit(note)}
+
+                        />
+
+                        <MdDelete
+                            className="text-secondary-400 cursor-pointer text-xl hover:brightness-170"
+                            onClick={() => onDelete(note.id)}
+                        />
+                    </div>
                 </div>
             </div>
         </div>
