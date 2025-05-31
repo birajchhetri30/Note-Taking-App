15c705c0a4e149f295bd5f848aac1f3c83b354e8
birajchhetri30 <birajchhetri3824@gmail.com>
2025-05-31 23:03:51 +0545

    Tailwind for Modal

--- Frontend/src/components/NoteList.jsx
+++ Frontend/src/components/NoteList.jsx
@@ -57,22 +57,8 @@ export default function NoteList({ notes, refreshNotes }) {
                 isOpen={modalOpen}
                 onRequestClose={() => setModalOpen(false)}
                 contentLabel='Edit note'
-                style={{
-                    overlay: {
-                        backgroundColor: 'rgba(0,0,0,0.2)',
-                    },
-                    content: {
-                        top: '50%',
-                        left: '50%',
-                        right: 'auto',
-                        bottom: 'auto',
-                        marginRight: '-50%',
-                        transform: 'translate(-50%, -50%)',
-                        width: '400px',
-                        padding: '20px',
-                        backgroundColor: 'black',
-                    }
-                }}
+                className="modal_style"
+                overlayClassName="modal_overlay_style"
             >
                 <AddNoteModal
                     onClose={closeModal}
