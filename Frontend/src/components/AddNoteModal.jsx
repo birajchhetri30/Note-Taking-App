dc24b5f7dd034e78af62d968281f5247ed4de806
birajchhetri30 <birajchhetri3824@gmail.com>
2025-05-31 23:04:40 +0545

    Modal design

--- Frontend/src/components/AddNoteModal.jsx
+++ Frontend/src/components/AddNoteModal.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import api from "../services/api";
 import CreatableSelect from 'react-select/creatable';
+import TextInput from "./TextInput";
 
 export default function AddNoteModal({ onClose, onNoteCreated, note, onNoteUpdated }) {
     const [form, setForm] = useState({ title: '', content: '', categories: [] });
@@ -97,22 +98,27 @@ export default function AddNoteModal({ onClose, onNoteCreated, note, onNoteUpdat
     };
 
     return (
-        <div>
-            <h3>{note ? 'Edit Note' : 'Add New Note'}</h3>
-            {error && <p style={{ color: 'red' }}>{error}</p>}
-            <form onSubmit={handleSubmit}>
-                <input
+        <div className="flex flex-col">
+            <h3
+                className="text-xl text-secondary-400 font-bold"
+            >{note ? 'Edit Note' : 'Add New Note'}</h3>
+
+            <hr className="border border-secondary-100 my-1" />
+
+            <form
+                className="mt-3"
+                onSubmit={handleSubmit}>
+
+                <TextInput
                     name='title'
-                    placeholder='Title'
                     value={form.title}
                     onChange={handleChange}
-                    maxLength={100}
-                    // required
-                    style={{ width: '100%', marginBottom: '10px' }}
                 />
-                <small>{form.title.length}/100</small>
+
+                <small className="flex justify-end mx-1 text-secondary-300">{form.title.length}/100</small>
 
                 <textarea
+                    className="max-h-40 min-h-40 h-40 mt-3 credentials_input outline-none"
                     name='content'
                     placeholder='Content'
                     value={form.content}
@@ -121,30 +127,73 @@ export default function AddNoteModal({ onClose, onNoteCreated, note, onNoteUpdat
                     style={{ width: '100%', marginBottom: '10px' }}
                 />
 
-                <label style={{ marginBottom: '5px', display: 'block' }}>Categories (optional)</label>
-                <CreatableSelect
-                    isMulti
-                    options={categoryOptions}
-                    value={form.categories}
-                    onChange={handleCategoryChange}
-                    placeholder='Select or create categories'
-                    onCreateOption={(inputValue) => {
-                        if (inputValue.length > 30) {
-                            setError('Category name must be less that 30 characters');
-                            return;
-                        }
-                        setError('');
-                        const newOption = { label: inputValue, value: inputValue };
-                        const updated = [...form.categories, newOption];
-                        setForm({ ...form, categories: updated });
-                    }}
-                    styles={{ container: base => ({ ...base, marginBottom: '10px' }) }}
-                />
+                <label className="mx-1 my-2 text-secondary-400">Categories (optional)</label>
 
-                <button type='submit' disabled={loading}>{note ? 'Update' : 'Create'}</button>
-                <button type='button' onClick={onClose} style={{ marginLeft: '10px' }}>Cancel</button>
+                <div className="lg:w-1/2 md:w-1/2">
+                    <CreatableSelect
+                        isMulti
+                        options={categoryOptions}
+                        value={form.categories}
+                        onChange={handleCategoryChange}
+                        placeholder='Select or create categories'
+                        onCreateOption={(inputValue) => {
+                            if (inputValue.length > 20) {
+                                setError('Category name must be less that 20 characters');
+                                return;
+                            }
+                            setError('');
+                            const newOption = { label: inputValue, value: inputValue };
+                            const updated = [...form.categories, newOption];
+                            setForm({ ...form, categories: updated });
+                        }}
+                        styles={styles}
+                    />
+                </div>
+
+                <div className="flex items-center justify-end">
+                    <button className='button mt-3' type='submit' disabled={loading}>{note ? 'Update' : 'Create'}</button>
+                    <button className='button mt-3 bg-transparent text-secondary-400' type='button' onClick={onClose} style={{ marginLeft: '10px' }}>Cancel</button>
+                </div>
             </form>
+
+            {error && <p className="self-center error_style">{error}</p>}
 
         </div>
     );
+}
+
+
+const styles = {
+    menu: (provided) => ({
+        ...provided,
+        backgroundColor: "#9c6644",
+        borderRadius: '0.75rem',
+        borderWidth: '2px',
+        borderColor: '#ddb892',
+        padding: '0.25rem',
+    }),
+    option: (provided, state) => ({
+        ...provided,
+        backgroundColor: state.isFocused ? '#ddb892' : 'transparent',
+        cursor: 'pointer',
+        borderRadius: '0.75rem',
+        padding: '0.5rem',
+        color: '#412512',
+        fontWeight: 'bold',
+
+    }),
+    control: (provided) => ({
+        ...provided,
+        backgroundColor: '#ddb892',
+        borderRadius: '0.75rem',
+        borderColor: '#b08968',
+    }),
+    multiValue: (provided) => ({
+        ...provided,
+        backgroundColor: "#7f5539",
+    }),
+    multiValueLabel: (provided) => ({
+        ...provided,
+        color: '#ede0d4',
+    }),
 }
